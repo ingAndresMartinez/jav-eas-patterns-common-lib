6ad7a752f4c96db444d0ab52068d077504fe51fd
ingAndresMartinez <ing.andresmartinezcobos@gmail.com>
2020-04-07 12:28:00 -0500

    Add new codes for authentication service

--- src/main/java/co/edu/javeriana/eas/patterns/common/enums/EExceptionCode.java
+++ src/main/java/co/edu/javeriana/eas/patterns/common/enums/EExceptionCode.java
@@ -2,8 +2,10 @@ package co.edu.javeriana.eas.patterns.common.enums;
 
 public enum EExceptionCode {
 
-    BLOCKING(433),
-    NON_BLOCKING(434);
+    BLOCKING(1000),
+    NON_BLOCKING(1001),
+    USER_OR_PASSWORD_INVALID(1002),
+    USER_BLOCKED(1003);
 
     private int code;
 
